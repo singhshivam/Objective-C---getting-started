ea33a82911e802ac7db739d1eca82d874895838b
singhshivam <singhshivam.172@gmail.com>
2020-05-30 12:21:46 -0500

    adding quotes

--- HelloWorld/ViewController.h
+++ HelloWorld/ViewController.h
@@ -11,8 +11,18 @@
 @interface ViewController : UIViewController
 // To declare a property:
 // 1. add @property keyword
-// 2. list property attributes
+// 2. list property attributes:
+//      a> nonatomic - increases performance at the cost of thread-safety
+//      b> strong - indicates that a pointer to the specified variable will
+//                  stay in memory as long as the object that holds the
+//                  pointer exists
+// 3. declare the type of property i.e. NSArray* (pointer to NSArray class)
+// 4. add property name i.e. myQuotes
 @property (nonatomic, strong) NSArray *myQuotes;
+
+// Once created you cannot add/remove elements in NSArray.
+// Therefore, use NSMutableArray
+@property (nonatomic, strong) NSMutableArray *movieQuotes;
 
 @end
 
--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -17,6 +17,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    // 1 - Add array of personal quotes
+    self.myQuotes = @[
+                        @"Live and let live",
+                        @"Don't cry over spilt milk",
+                        @"Always look on the bright side of life",
+                        @"Nobody's perfect",
+                        @"Can't see the woods for the trees",
+                        @"Better to have loved and lost then not loved at all",
+                        @"The early bird catches the worm",
+                        @"As slow as a wet week"
+                        ];
 }
 
 
